939087f39c1ad99a06aa2ab3be738acf790dfcea
rtty <ryantsui786@gmail.com>
2022-09-01 20:17:56 +0800

    added handedness and angle detection between landmarks

--- app.py
+++ app.py
@@ -1,19 +1,59 @@
 import mediapipe as mp
 import cv2
+from matplotlib import pyplot as plt
 import numpy as np
 import uuid
 import os
 
+# Initialises the mediapipe classes
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-# Outputting images
-os.mkdir("Output Images")
+jointList = [[8, 7, 6], [12, 11, 10], [4, 3, 2], [1, 0, 5]]
+
+def getLabel(index, hand, results):
+    output = None
+    for idx, classification in enumerate(results.multi_handedness):
+        if classification.classification[0].index == index:
+
+            label = classification.classification[0].index
+            score = classification.classification[0].score
+
+            # Text is labels and score
+            text = '{} {}'.format(label, round(score, 2))
+
+            coords = tuple(np.multiply(
+                np.array((hand.landmark[mp_hands.HandLandmark.WRIST].x, hand.landmark[mp_hands.HandLandmark.WRIST].y)), 
+                [width, height]).astype(int))
+
+            output = text, coords
+
+    return output
+
+def drawFingerAngles(image, results, jointList):
+
+    for hand in results.multi_hand_landmarks:
+        for joint in jointList:
+            a = np.array([hand.landmark[joint[0]].x, hand.landmark[joint[0]].y]) # First coord
+            b = np.array([hand.landmark[joint[1]].x, hand.landmark[joint[1]].y]) # Second coord
+            c = np.array([hand.landmark[joint[2]].x, hand.landmark[joint[2]].y]) # Third coord
+
+            radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+            angle = np.abs(radians*180.0/np.pi)
+
+            if angle > 180.0:
+                angle = 360 - angle
+
+            cv2.putText(image, str(round(angle, 2)), tuple(np.multiply(b, [width, height]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+    return image
 
 cap = cv2.VideoCapture(0)
 
+# Self explanatory
 with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
+        width, height = cap.get(3), cap.get(4)
 
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
@@ -38,14 +78,20 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
                     mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
                     mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2)
                     )
-        # cv2.imwrite(os.path.join('Output Images', '{}.jpg'.format(uuid.uuid1())), image)
+
+                if getLabel(num, hand, results):
+                    text, coord = getLabel(num, hand, results)
+                    n = text[0]
+                    n = 'left' if n == '1' else 'right'
+                    n = n + text[1:]
+                    cv2.putText(image, n, coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            drawFingerAngles(image, results, jointList)
 
         cv2.imshow('Hand Tracking', image)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-
-
 
 cap.release()
 cv2.destroyAllWindows()
